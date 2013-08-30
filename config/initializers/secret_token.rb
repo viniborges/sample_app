7f904084bf97ef82747b2fb9f84bcdbc81ab5dca
viniborges <viniborges@yahoo.com.br>
2013-08-30 00:17:12 -0300

    Commit inicial: configurado Gemfile, postgresql e gerado rspec.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2c0aff12cb7e5d36de1f10e935cdd86f21f18d51f9657f03f24f930b064c986592b9d4ee112405f4624d095a65da7ff391e5b13f63450ebfcd78322a732f6c38'
